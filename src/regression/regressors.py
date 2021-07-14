8b7a9a8fffdc9690d8ae051caff369a5df7e4bef
A761684 <xfgaka3ghqgll6e4lx4ni7gmsvebwfh4icpoyqm3jt2htahrsd5q>
2021-07-14 23:35:35 +0200

    custom Multivariate linear regression

--- src/regression/regressors.py
+++ src/regression/regressors.py
@@ -1,58 +1,107 @@
 import numpy as np
-import pandas as pd
 
 class LinearRegressor:
-    def __init__(self, learning_rate: int = 0.01, iterations: int = 500):
+    def __init__(self, learning_rate: int = 0.005, iterations: int = 500, verbose: bool = False):
         self.learning_rate = learning_rate
         self.iterations = iterations
+        self.vebose = verbose
+        self.cost_tracker =[]
+        self.rsme_tracker =[]
+        self.bias, self.weights = None, None
 
-    def fit(self,X:pd.DataFrame,Y):
+    def fit(self,X:np.ndarray,Y:np.ndarray):
         '''
         :param X: This a dataframe with all the independent variables
-        :type X: Panda.DataFrame
-        :param Y: This is an array of
-        :type Y:
-        :return:
-        :rtype:
+        :type X: np.ndarray
+        :param Y: This is an array of target/dependent variables
+        :type Y: np.ndarray
+        :return: self
+        :rtype: LinearRegressor object
         '''
-
         self.m , self.n = X.shape
-        self.bias = 0
-        self.W = np.zeros(self.n)
         self.X = X
         self.Y = Y
+        self.bias = 1
+        self.weights = np.zeros(self.n)
 
+        #todo: assert x and y shapes
 
-        #todo: assert x and y
+        try:
+            for _ in range(self.iterations):
+                    self.update_bias_weight()
+                    loss = self._mse(self.X,self.Y)
+                    rmse_loss = self._rmse(self.X,self.Y)
+                    if self.vebose:
+                        print(f'Current RMSE is {rmse_loss} @ {_}')
+                    self.cost_tracker.append((loss, _))
+                    self.rsme_tracker.append((rmse_loss, _))
+        except Exception as e:
+            print(e)
+            return self
 
-        for _ in range(self.iterations):
-            self.update_bias_weight()
         return self
+
+
 
     def update_bias_weight(self):
         '''
-        This method update Weights and biases based on gradient and learning rate
+        This method update Weights and biases using  gradient descent
+        :return: self
+        :rtype: LinearRegressor object
+        '''
+        self.dW , self.dbias = self.calculate_partial_derivative()
+        self.weights = self.weights - (self.learning_rate * self.dW)
+        self.bias = self.bias - (self.learning_rate * self.dbias)
+
+        return self
+
+    def calculate_partial_derivative(self):
+        '''
+        This method calculate the partial derivative for weights and bias
         :return: self
         :rtype: LinearRegressor object
         '''
         Y_hat = self.predict(self.X)
-        self.dW , self.dbias = self.calculate_gradient(Y_hat)
 
-        self.W = self.W - self.learning_rate * self.dW
-        self.bias = self.bias - self.learning_rate * self.dbias
-        return self
+        dWeights = (2 * (self.X.T).dot(Y_hat - self.Y)) / self.m
+        dBias = 2 * np.sum(Y_hat - self.Y) / self.m
 
-    def calculate_gradient(self, Y_hat):
-        '''
-        This method calculate the gradients of weights and biases
-        :param Y_hat:
-        :type Y_hat:
-        :return: self
-        :rtype: LinearRegressor object
-        '''
-        dWeights = - (2 * (self.X.T).dot(self.Y - Y_hat)) / self.m
-        dBias = - 2 * np.sum(self.Y - Y_hat) / self.m
         return dWeights, dBias
 
     def predict(self, X):
-        return X.dot(self.W)+self.bias
+        """
+        This method use leared theta/weights and bias to make predictions
+        :param X: Independept variables
+        :type X: np.ndarray
+        :return: Prediction values
+        :rtype: np.ndarray
+        """
+        return X.dot(self.weights)+self.bias
+
+    def _mse(self, X, Y):
+        """
+        This method calculate mean squared error of given X and Y for weights and bias of model
+        :param X: This a dataframe with all the independent variables
+        :type X: np.ndarray
+        :param Y:
+        :type Y: np.ndarray
+        :return: mean squared error
+        :rtype: np.float64
+        """
+        m, n = X.shape
+        y_hat = self.predict(X)
+        return ((Y - y_hat) ** 2).sum() / m
+
+    def _rmse(self, X, Y):
+        """
+        This method calculates root mean squared error of given X and Y for weights and bias of model
+
+        :param X: This a dataframe with all the independent variables
+        :type X: np.ndarray
+        :param Y: This is an array of target/dependent variables
+        :type Y: np.ndarray
+        :return: Root mean suqred error
+        :rtype: np.float64
+        """
+        y_hat = self.predict(X)
+        return np.sqrt(np.mean((y_hat - Y) ** 2))
