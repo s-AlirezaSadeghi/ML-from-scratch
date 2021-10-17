d05e0278abab4645d8796c247e47995ea1de53c7
S.A.S <alireza13613@gmail.com>
2021-10-18 00:29:58 +0200

    Added test for metrics of Entropy and MSE Added mypy config Added packages to requirements Added directories to .gitignore Added ENV setup instruction to README.MD

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# ML-from-scratch
+# Machine learning from scratch
 
 In this repository I implement a number of Machine Learning algorithms from scratch. 
-I also compare their performance to scikit learn implementations.
+In addition, I script a performance comparison against  scikit learn implementations.
 
 
+### Environment setup
+**Warning**: if you have had the environment installed previously then execute "conda remove --all --name  ml-from-scratch"</br>
 
-To install the environment for this project follow the instructions below.</br>
-1- go to environment directory</br>
-1b- if you have the environment installed previously then execute "conda remove --name ml-from-scratch --all"</br>
-2- excecute "conda env create --file environment.yml"</br>
-3- then activate the environment "conda activate ml-from-scratch"
+To install the environment for this project follow instructions below.</br>
+1- cd into the environment directory</br>
+2- Execute "conda env create --file environment.yml"</br>
+3- if all goes well, you can activate the environment yb executing "conda activate ml-from-scratch"
 
-conda list -e > requirements.txt
+To run tests execute " python -m pytest tests -v"
+
 
--- src/utility/metrics.py
+++ src/utility/metrics.py
@@ -1,35 +1,46 @@
 import numpy as np
 from scipy import stats
-from collections import Counter
 
 
-
-def mse(y_hat, y_true):
-    assert len(y_true) ==len(y_hat), "Lenght of Y pred and Y True don't match!"
+def mse(y_hat: np.ndarray, y_true: np.ndarray):
+    assert isinstance(y_hat, np.ndarray), "predictions should be numpy array"
+    assert isinstance(y_true, np.ndarray), "True values should be numpy array"
+    assert len(y_true) == len(y_hat), "Lenght of Y pred and Y True don't match!"
     return ((y_true - y_hat) ** 2).sum() / len(y_true)
 
-def rmse(y_hat, y_true):
-    assert len(y_true) ==len(y_hat), "Lenght of Y pred and Y True don't match!"
+
+def rmse(y_hat: np.ndarray, y_true: np.ndarray):
+    assert isinstance(y_hat, np.ndarray), "predictions should be numpy array"
+    assert isinstance(y_true, np.ndarray), "True values should be numpy array"
+    assert len(y_true) == len(y_hat), "Lenght of Y pred and Y True don't match!"
     return np.sqrt(np.mean((y_hat - y_true) ** 2))
 
 
-def mae(y_hat, y_true):
-    assert len(y_true) ==len(y_hat), "Lenght of Y pred and Y True don't match!"
+def mae(y_hat: np.ndarray, y_true: np.ndarray):
+    assert isinstance(y_hat, np.ndarray), "predictions should be numpy array"
+    assert isinstance(y_true, np.ndarray), "True values should be numpy array"
+    assert len(y_true) == len(y_hat), "Lenght of Y pred and Y True don't match!"
     return np.average(np.abs(y_hat - y_true))
 
-def cross_entropy(y_hat,y_true,epsilon=1e-12):
 
-    assert len(y_true) ==len(y_hat), "Lenght of Y pred and Y True don't match!"
-    assert (y_hat >= 0).all() and (y_hat <= 1).all() , 'Y pred should be between 0 and 1'
-    #todo: assert whether target is either 0-1 values
+def cross_entropy(y_hat: np.ndarray, y_true: np.ndarray, epsilon=1e-12):
+    assert isinstance(y_hat, np.ndarray), "predictions should be numpy array"
+    assert isinstance(y_true, np.ndarray), "True values should be numpy array"
+    assert len(y_true) == len(y_hat), "Lenght of Y pred and Y True don't match!"
+    assert (y_hat >= 0).all() and (y_hat <= 1).all(), 'Y pred should be between 0 and 1'
+    # todo: assert whether target is either 0-1 values
     preds = np.clip(y_hat, epsilon, 1. - epsilon)
     return -np.sum(y_true * np.log(preds + 1e-9)) / len(y_hat)
 
-def accuracy(y_hat,y_true):
-    assert len(y_true) ==len(y_hat), "Lenght of Y pred and Y True don't match!"
-    return np.sum(y_true==y_hat)/len(y_true)
 
-#Todo : finish this
+def accuracy(y_hat: np.ndarray, y_true: np.ndarray):
+    assert isinstance(y_hat, np.ndarray), "predictions should be numpy array"
+    assert isinstance(y_true, np.ndarray), "True values should be numpy array"
+    assert len(y_true) == len(y_hat), "Lenght of Y pred and Y True don't match!"
+    return np.sum(y_true == y_hat) / len(y_true)
+
+
+# Todo : finish this
 def logloss():
     """
     Log loss, aka logistic loss or cross-entropy loss.
@@ -39,24 +50,20 @@ def logloss():
     """
     ...
 
-#Todo : finish this
+
+# Todo : finish this
 def gini():
     ...
 
-#Todo : finish this
+
+# Todo : finish this
 def partial_derivative():
     ...
+
 
 def entropy(data: list):
     """Calculates entropy of the passed `List`
     """
-    value, counts = np.unique(label_list, return_counts=True)  # counts occurrence of each value
+    assert isinstance(data, list), "Data should be a List type"
+    value, counts = np.unique(data, return_counts=True)  # counts occurrence of each value
     return stats.entropy(counts, base=2)  # get entropy from counts
-
-
-label_list = ["a","a","b","b","b","b"]
-value, counts = np.unique(label_list, return_counts=True)
-
-entropy(label_list)
-
-stats.entropy(counts, base=2)
