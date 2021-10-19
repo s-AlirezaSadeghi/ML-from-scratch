5fdf687ee9a03f46588a6b17f3a206f47e5a80c0
A761684 <xfgaka3ghqgll6e4lx4ni7gmsvebwfh4icpoyqm3jt2htahrsd5q>
2021-10-19 11:45:07 +0200

    Starting CART - with Classification

--- src/cart/decision_tree.py
+++ src/cart/decision_tree.py
@@ -1,64 +1,9 @@
 import numpy as np
 from collections import Counter
+from scipy.stats import entropy
 from typing import List
+from math import e
 
-
-# Calculate the Gini index for a split dataset
-def gini_index(groups, classes):
-    # count all samples at split point
-    n_instances = float(sum([len(group) for group in groups]))
-    # sum weighted Gini index for each group
-    gini = 0.0
-    for group in groups:
-        size = float(len(group))
-        # avoid divide by zero
-        if size == 0:
-            continue
-        score = 0.0
-        # score the group based on the score for each class
-        for class_val in classes:
-            p = [row[-1] for row in group].count(class_val) / size
-            score += p * p
-        # weight the group score by its relative size
-        gini += (1.0 - score) * (size / n_instances)
-    return gini
-
-def gini(data):
-    labels = Counter(data)
-    impurity = 1
-    for label in labels:
-        # go over each label in counts
-        prob = labels[label] / float(len(data))
-        impurity -= prob ** 2
-    return impurity
-
-
-def information_gain(left, right, current_uncertainty):
-    p = float(len(left)) / (len(left) + len(right))
-    return  current_uncertainty - p * gini(left[:, -1]) - (1 - p) * gini(right[:,-1])
-
-# def gini(arr):
-#     array = arr.flatten()
-#     array = array.astype('float64')
-#     if np.amin(array) < 0:
-#         # Values cannot be negative:
-#         array -= np.amin(array)
-#     # Values cannot be 0:
-#     array += 0.0000001
-#     # Values must be sorted:
-#     array = np.sort(array)
-#     # Index per array element:
-#     index = np.arange(1, array.shape[0] + 1)
-#     # Number of array elements:
-#     n = array.shape[0]
-#     # Gini coefficient:
-#     return (np.sum((2 * index - n - 1) * array)) / (n * np.sum(array))
-
-#https://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
-#https://github.com/random-forests/tutorials/blob/master/decision_tree.py
-#https://github.com/random-forests/tutorials/blob/master/decision_tree.ipynb
-#https://www.youtube.com/watch?v=LDRbO9a6XPU
-#https://github.com/oliviaguest/gini/blob/master/gini.py
 
 class SplitCondition:
     def __init__(self, value, col: int, col_names: List):
@@ -68,108 +13,138 @@ class SplitCondition:
 
     def match(self, data):
         subspace = data[:, self.col]
-        if np.issubdtype(type(self.value),int) or isinstance(self.value, float):
+        if np.issubdtype(type(self.value), np.signedinteger) or isinstance(self.value, float):
             return subspace >= self.value
-        elif isinstance(self.value,str):
+        elif isinstance(self.value, str):
             return subspace == self.value
 
     def __repr__(self):
         condition = "=="
-        if np.issubdtype(type(self.value),int) or isinstance(self.value, float):
+        if np.issubdtype(type(self.value), np.signedinteger) or isinstance(self.value, float):
             condition = ">="
         return f"Is {self.col_names[self.col]} {condition} {str(self.value)}"
 
 
 class ClassificationTree:
-    def __init__(self,headers: List, impurity: str = 'gini', min_samples_split: int = 10, max_depth: int = 50 ):
+    def __init__(self, headers: List, impurity: str = 'gini', min_samples_split: int = 10, max_depth: int = 50):
         self.impurity = impurity
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
-        self.headers= headers
+        self.headers = headers
 
-
-    #Get distinct values of an array
+    # return distinct values of an array
     def unique_vals(self, rows, col):
         return np.unique(rows[:, col])
 
-    #parition data based on a condition
-    def parition(self,data,condition):
-        true_case = data[condition.match(data)]
-        false_case = data[~(condition.match(data))]
+    # paritions/splits data based on a condition
+    def parition(self, data, condition):
+        '''
+        splits data based on the given condition into 2 paritions
+        :param data: an array of data to be splitted
+        :type data: np.array
+        :param condition: a condition which determines the how to split data
+        :type condition:  SplitCondition class
+        :return: two parition of data splitted by condition passed to the method
+        :rtype: (np.array,np.array)
+        '''
+        x, y = data
+        true_case = (x[condition.match(x)],y[condition.match(x)])
+        false_case = (x[~(condition.match(x))],y[~(condition.match(x))])
         return true_case, false_case
 
-    def gini(self,data):
-        labels = Counter(data)
-        impurity = 1
-        for label in labels:
-            # go over each label in counts
-            prob = labels[label] / float(len(data))
-            impurity -= prob ** 2
-        return impurity
+    # https: // www.youtube.com / watch?v = LDRbO9a6XPU
+    # https: // machinelearningmastery.com / implement - decision - tree - algorithm - scratch - python /
+
+    def gini(self, data):
+        '''
+        Calculate gini index for the given array
+        :param data: an array of values
+        :type data: np.array
+        :return: gini index of input array
+        :rtype: float
+        '''
+        labels, label_freqs = np.unique(data, return_counts=True)
+        return 1 - (np.power(label_freqs / label_freqs.sum(), 2)).sum()
 
     #   information gain
-    def information_gain(self,left,right,current_uncertainty):
-        p = float(len(left)) / (len(self.left) + len(self.right))
-        return current_uncertainty - p * gini(left[:, -1]) - (1 - p) * gini(right[:, -1])
+    def information_gain(self, left, right, current_uncertainty):
+        '''
+        Calculates the information gain (weighted gini ) based on the splits ( right and left ) compared to the existing uncertainity
+        :param left: left split of tree node
+        :type left: np.array
+        :param right: right split of tree node
+        :type right:  np.array
+        :param current_uncertainty: gini index of array before splits
+        :type current_uncertainty: float
+        :return: quantity of infortmation gain by making splits rightr and left based on the condition
+        :rtype: float
+        '''
+        # calculate left split  weight - right split weight is 1- left split
+        p = float(len(left)) / (len(left) + len(right))
+        return current_uncertainty - p * self.gini(left[1]) - (1 - p) * self.gini(right[1])
 
-
-
-    def best_split(self,data):
+    def best_split(self, x: np.array, y: np.array):
+        '''
+        determines the best condition to split a node into two paritions using gini and information gain iteratively
+        :param data: node data (whether root or sub-root )
+        :type data: np.ndarray
+        :return: a condition/question  based on features and their distinct values which leads to highest information gain after split to
+        left and right split
+        :rtype: (float,SplitCondition)
+        '''
         best_gain = 0
         best_cond = None
-        current_uncertainty = self.gini(data[:,-1])
-        n_features = data.shape[1]-1
-        for col in range(n_features):
-            distinct_vals = Counter(data[:,col])
-            for val in distinct_vals:
-                cond = SplitCondition(val,col,self.headers)
-                true_case , false_case = self.parition(data,cond )
+        # Calculate gini score before split
+        current_uncertainty = self.gini(y)
 
-                if len(true_case) == 0 or len(false_case) == 0:
+        # Excluding targer var from n_features
+        n_features = x.shape[1]
+
+        # find split condition that increase information_gain
+        for col in range(n_features):
+            distinct_vals = Counter(x[:, col])
+            for val in distinct_vals:
+                # split condition
+                cond = SplitCondition(val, col, self.headers)
+                # Partition data and return Tuple of split in (x,y) format
+                true_case, false_case = self.parition((x, y), cond)
+
+                if len(true_case[0]) == 0 or len(false_case[0]) == 0:
                     continue
 
-                gain = information_gain(true_case,false_case,current_uncertainty)
+                gain = self.information_gain(true_case, false_case, current_uncertainty)
 
                 if gain >= best_gain:
                     best_gain, best_cond = gain, cond
         return best_gain, best_cond
 
+    def fit(self, x: np.array, y: np.array):
 
-#sample data creation
-cardiac_data = np.random.randint(19, 150, size=(45, 2),dtype=int)
-heart_attack = np.random.choice([0, 1], size=(45, 1), p=[0.90, 0.10])
-training_data = np.column_stack((cardiac_data,heart_attack))
+        # get best split with most gain
+        gain, condition = self.best_split(x, y)
 
-# calculate starting current_uncertainty
-current_uncertainty = gini(training_data[:,-1])
-print(f'current_uncertainty is : {current_uncertainty}')
-print(Counter(training_data[:,-1]))
+        if gain == 0:
+            return Leaf(x, y)
 
+        true_data, false_data = ct.parition(x, y, condition)
 
-# for _ in range(1,2):
-#     cond = SplitCondition(147,0,['heart_rate','age','heart_attack'])
-#     print(cond.__repr__())
-#
-#     true_case = training_data[cond.match(training_data)]
-#     false_case = training_data[~(cond.match(training_data))]
-#     print(f'info gain is {information_gain(true_case, false_case, current_uncertainty)}')
-#
-# true_case.shape
-# false_case.shape
-ct = ClassificationTree(headers=['heart_rate','age','heart_attack'])
+        # Recursively build the true branch.
+        left_branch = self.tree_builder(true_data)
 
-ct.best_split(training_data)
+        # Recursively build the false branch.
+        right_branch = self.tree_builder(false_data)
+
+        return Decision_Node(condition, left_branch, right_branch)
+
 
 class Leaf:
     """A Leaf node classifies data.
-
-    This holds a dictionary of class (e.g., "Apple") -> number of times
+    Contains a dictionary of target class(es) (e.g., "Apple") -> number of times
     it appears in the rows from the training data that reach this leaf.
     """
 
     def __init__(self, data):
-        self.predictions = Counter(data[:,-1])
-
+        self.predictions = Counter(data[:, -1])
 
 
 class Decision_Node:
@@ -186,47 +161,121 @@ class Decision_Node:
         self.true_branch = true_branch
         self.false_branch = false_branch
 
-def tree_builder(data):
-
-    ct = ClassificationTree(headers=['heart_rate', 'age', 'heart_attack'])
-    # get best split with most gain
-    gain , condition = ct.best_split(data)
-
-    if gain == 0:
-        return Leaf(data)
-
-    true_data, false_data = ct.parition(data, condition)
-
-    # Recursively build the true branch.
-    true_branch = tree_builder(true_data)
-
-    # Recursively build the false branch.
-    false_branch = tree_builder(false_data)
-
-    return Decision_Node(condition, true_branch, false_branch)
+    # def predict(self,data)
 
 
-my_tree = tree_builder(training_data)
+# def tree_builder(data: np.array, labels: List):
+#     ct = ClassificationTree(headers=labels)
+#     # get best split with most gain
+#     gain, condition = ct.best_split(data)
+#
+#     if gain == 0:
+#         return Leaf(data)
+#
+#     true_data, false_data = ct.parition(data, condition)
+#
+#     # Recursively build the true branch.
+#     left_branch = tree_builder(true_data)
+#
+#     # Recursively build the false branch.
+#     right_branch = tree_builder(false_data)
+#
+#     return Decision_Node(condition, left_branch, right_branch)
+
 
 def print_tree(node, spacing=""):
     """World's most elegant tree printing function."""
 
     # Base case: we've reached a leaf
     if isinstance(node, Leaf):
-        print (spacing + "Predict", node.predictions)
+        print(spacing + "Predict", node.predictions)
         return
 
     # Print the question at this node
-    print (spacing + str(node.question))
+    print(spacing + str(node.question))
 
     # Call this function recursively on the true branch
-    print (spacing + '--> True:')
+    print(spacing + '--> True:')
     print_tree(node.true_branch, spacing + "  ")
 
     # Call this function recursively on the false branch
-    print (spacing + '--> False:')
+    print(spacing + '--> False:')
     print_tree(node.false_branch, spacing + "  ")
 
+
+def gini2(data):
+    labels, label_freqs = np.unique(data, return_counts=True)
+    return 1 - (np.power(label_freqs / label_freqs.sum(), 2)).sum()
+
+
+def gini3(data):
+    labels, label_freqs = np.unique(data, return_counts=True)
+    prob_impurity = label_freqs / label_freqs.sum()
+    return 2 * np.prod(prob_impurity)
+
+
+def entropy_scipy(data):
+    labels, label_freqs = np.unique(data, return_counts=True)
+    probabilities = label_freqs / label_freqs.sum()
+    return entropy(probabilities)
+
+
+def entropy_numpy(data):
+    labels, label_freqs = np.unique(data, return_counts=True)
+    probabilities = label_freqs / label_freqs.sum()
+    return -(probabilities * np.log(probabilities) / np.log(e)).sum()
+
+
+# sample data creation
+num_rows = 50
+cardiac_data = np.random.randint(50, 112, size=(num_rows, 1), dtype=int)
+age_data = np.random.randint(19, 85, size=(num_rows, 1), dtype=int)
+heart_attack = np.random.choice([0, 1], size=(num_rows, 1), p=[0.75, 0.25])
+training_data = np.column_stack((cardiac_data, age_data, heart_attack))
+
+
+x = np.column_stack((cardiac_data, age_data))
+y=  heart_attack
+
+# calculate starting current_uncertainty
+current_uncertainty = gini2(training_data[:, -1])
+# print(f'initial information gain  is : {current_uncertainty}')
+print(Counter(training_data[:, -1]))
+
+# gini(training_data[:,-1])
+gini2(training_data[:, -1])
+gini3(training_data[:, -1])
+entropy_scipy(training_data[:, -1])
+entropy_numpy(training_data[:, -1])
+
+# ct = ClassificationTree(headers=['heart_rate','age','heart_attack'])
+# ct.best_split(training_data)
+
+ct = ClassificationTree(['heart_rate','age','heart_attack'])
+ct.fit(x,y)
+print('x')
+# my_tree = tree_builder(data=training_data, labels=['heart_rate', 'age', 'heart_attack'])
 print_tree(my_tree)
+print(f'initial information gain  is : {current_uncertainty}')
 
 print('Job done!')
+
+
+def classify(row, node):
+    """See the 'rules of recursion' above."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    # Decide whether to follow the true-branch or the false-branch.
+    # Compare the feature / value stored in the node,
+    # to the example we're considering.
+    if node.question.match(row):
+        return classify(row, node.true_branch)
+    else:
+        return classify(row, node.false_branch)
+
+
+print(training_data[0, :-1])
+classify(training_data[0], my_tree)
